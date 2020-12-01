d1abafce6867fbb512dc6e0806a796b8ea3a4559
hbrault <henri.brault@ecole.ensicaen.fr>
2020-12-01 18:08:20 +0100

    added instructions to setup a basic sqlite database

--- db/setup.py
+++ db/setup.py
@@ -1,7 +1,7 @@
 import sqlite3
+import os
 
-
-conn = sqlite3.connect('estate.db')
+conn = sqlite3.connect("estate.db")
 
 cursor = conn.cursor()
 
@@ -9,27 +9,38 @@ cursor = conn.cursor()
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS users(
     id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-    name TEXT,
-    first_name TEXT,
-    date_of_birth INTEGER
-)
+    name TEXT NOT NULL,
+    first_name TEXT NOT NULL
+);
+
+
+
+
+
 """)
 
-#Create table properties
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS properties(
     id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+    user_id INTEGER,
+    FOREIGN KEY(user_id) REFERENCES users(id)
     name TEXT,
-    surface_area INTEGER,
-    date_of_birth INTEGER
-)
+    location TEXT,
+    surface_area INTEGER
+);
 """)
 
 
+#Create table properties
+# cursor.execute("""
+#
+# """)
+
+# #Add Henri Brault user
+# cursor.execute("INSERT INTO users (name,first_name) VALUES ('Brault','Henri')")
+#
+# cursor.execute("SELECT * FROM users")
+# print(cursor.fetchone())
+
 conn.commit()
-
-
-
-
-
 conn.close()
