ce2e805ae8df0f10d2c2fed25afc80372752b69f
hbrault <henri.brault@ecole.ensicaen.fr>
2020-12-05 19:42:34 +0100

    removed old sqlite db file, now using sqlalchemy + working User queries PUT GET and PATCH fully fonctionning

--- api/main.py
+++ api/main.py
@@ -1,18 +1,161 @@
-from flask import Flask
-from flask_restful import Api, Resource
+from flask import Flask,request
+from flask_restful import Api, Resource, reqparse, fields, marshal_with, abort
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 api = Api(app)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
 
 
-class HelloWorld(Resource):
-	def get(self):
-		return{"data" : "Hello World"}
+class UserModel(db.Model):
+	__tablename__ = "users"
+
+	id = db.Column(db.Integer,primary_key=True)
+	last_name = db.Column(db.String(50),nullable=False)
+	first_name = db.Column(db.String(50),nullable=False)
+	date_of_birth = db.Column(db.String(10),nullable = False)
+
+	def __repr__(self):
+		return f"User (last_name = {last_name}, first_name = {first_name}, date_of_birth = {date_of_birth})"
+
+# class HousesModel(db.Model):
+# 	__tablename__ = "houses"
+
+# 	id = db.Column(db.Integer,primary_key=True)
+# 	user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+# 	name = db.Column(db.String(db))
+# 	location = db.Column(db.String(50),nullable=False)
+# 	surface_area = db.Column(db.Integer,nullable=True)
+
+# 	def __repr__(self):
+# 		return f"House (name = {name}, location = {location}, surface_area = {surface_area})"
+
+# class RoomsModel(db.Model):
+# 	__tablename__ = "rooms"
+
+# 	id = db.Column(db.Integer,primary_key=True)
+# 	fk = db.Column(db.Integer,db.ForeignKey("houses.id"))
+# 	name = db.Column(db.String(50),nullable=False)
+# 	surface_area = db.Column(db.Integer,nullable=True)
+
+# 	def __repr__(self):
+# 		return f"Room (name = {name}, surface_area = {surface_area})"
+
+# db.create_all()
+
+##
 
 
-api.add_resource(HelloWorld,"/helloworld")
+# house_put_args = reqparse.RequestParser()
+# house_put_args.add_argument("house", type=str,help="Name of house required",required=True)
+
+# house_resource_fields = {
+# 	"id" : fields.Integer,
+# 	"user_id" : fields.Integer,
+# 	"name" : fields.String,
+# 	"location" : fields.String,
+# 	"surface_area" : fields.Integer,
+# }
+# class HouseExplorer(Resource):
+# 	@marshal_with(house_resource_fields)
+# 	def get(self,name):
+# 		results = HousesModel.query.filter_by(name=name)
+# 		return results
+
+# class User(Resource):
+# 	def put(self,user_id):
+# 		args = house_put_args.parse_args()
+# 		return {user_id : args}
+
+##
+
+user_put_args = reqparse.RequestParser()
+user_put_args.add_argument("last_name", type=str,help="Family name of user required",required=True)
+user_put_args.add_argument("first_name", type=str,help="First name of user required",required=True)
+user_put_args.add_argument("date_of_birth", type=str,help="Date of birth of user required",required=True)
+
+user_update_args = reqparse.RequestParser()
+user_update_args.add_argument("last_name", type=str,help="Family name of user required",required=False)
+user_update_args.add_argument("first_name", type=str,help="First name of user required",required=False)
+user_update_args.add_argument("date_of_birth", type=str,help="Date of birth of user required",required=False)
+
+user_fields = {
+	"id" : fields.Integer,
+	"last_name" : fields.String,
+	"first_name" : fields.String,
+	"date_of_birth" : fields.String
+}
+class User(Resource):
+	@marshal_with(user_fields)
+	def put(self,user_id):
+		result = UserModel.query.filter_by(id=user_id).first()
+		if result:
+			abort(409,message="User id already exists")
+
+		args = user_put_args.parse_args()
+		user = UserModel(id=user_id,last_name=args["last_name"],first_name=args["first_name"],date_of_birth=args["date_of_birth"])
+		db.session.add(user)
+		db.session.commit()
+		return user, 201
+
+	@marshal_with(user_fields)
+	def get(self,user_id):
+		result = UserModel.query.filter_by(id=user_id).first()
+		if not result:
+			abort(404, message="User does not exist")
+		return result
+
+	@marshal_with(user_fields)
+	def patch(self,user_id):
+		args = user_update_args.parse_args()
+		result = UserModel.query.filter_by(id=user_id).first()
+		if not result:
+			abort(404, message="User does not exist")
+
+		if args["first_name"]:
+			result.first_name = args["first_name"]
+		if args["last_name"]:
+			result.last_name = args["last_name"]
+		if args["date_of_birth"]:
+			result.date_of_birth = args["date_of_birth"]
+
+		db.session.commit()
+		return result
+		
 
 
+
+api.add_resource(User,"/user/<user_id>")
+##
+
+# house_put_args = reqparse.RequestParser()
+# house_put_args.add_argument("user_id", type=str,help="ID of user required",required=True)
+# house_put_args.add_argument("name", type=str,help="Name of house required",required=True)
+# house_put_args.add_argument("location", type=str,help="Location of house required",required=True)
+# house_put_args.add_argument("surface_area", type=int,help="Surface area of house required")
+
+
+# house_fields = {
+# 	"id" : fields.Integer,
+# 	"user_id" : fields.Integer,
+# 	"name" : fields.String,
+# 	"location" : fields.String,
+# 	"surface_area" : fields.Integer,
+# }
+# class House(Resource):
+# 	@marshal_with(house_fields)
+# 	def put(self,user_id):
+# 		args = house_put_args.parse_args()
+# 		house = HousesModel(user_id=args["user_id"],name=args["name"],location=args["location"],surface_area=args["surface_area"])
+# 		db.session.add(house)
+# 		db.session.commit()
+# 		return house, 201
+
+
+
+# api.add_resource(House,"/browse/<string:name>")
+# api.add_resource(User,"/account/<user_id>")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- api/test.py
+++ api/test.py
@@ -2,5 +2,18 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "helloworld")
-print(response.json())
+
+# response = requests.put(BASE + "user/0",{
+# 	"last_name" : "Brault",
+# 	"first_name" : "Henri",
+# 	"date_of_birth" : "15/04/1999"})
+# print(response.json())
+
+# response = requests.put(BASE + "user/1",{
+# 	"last_name" : "Zidane",
+# 	"first_name" : "Zinédine",
+# 	"date_of_birth" : "23/06/1972"})
+# print(response.json())
+
+input("Query puts done, press any key")
+
